bacbe18c51b94bd3e75722d0366f8234455ae2c8
ikramsinapoy <59946070+ikramsinapoy@users.noreply.github.com>
2020-01-16 12:07:45 +0700

    tambah program

---
+++ test/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "ikram sinapoy" << endl;
+    cout << "1301193341" << endl;
+    int i;
+    int i2;
+
+    cout <<"angka i = " <<i<< "\n";
+    cout <<"angka i2 = " <<i<< "\n";
+
+    return 0;
+}
